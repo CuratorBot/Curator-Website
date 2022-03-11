876dc972f50d4765df1be60bca0fa697bdb63ef4
Cindr Mon <cindrmon@protonmail.com>
2022-03-12 00:44:59 +0800

    update dashboard layout
    - create dynamic routing for different dashboards.
    - therewasanattempt to fix unknown-at-rules.

--- src/views/Dashboard.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" setup></script>
-<template>
-  <div class="dashboard">
-    <h1>Dashboard</h1>
-  </div>
-</template>
-<style></style>
---
+++ src/views/dashboard/[serverID]/settings.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+defineProps<{ serverID: String }>()
+
+</script>
+
+<template>
+    <div>
+        <h1>Server Settings</h1>
+        <p>Server ID: {{ serverID }}</p>
+    </div>
+</template>
